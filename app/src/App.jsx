cfbab83e333bfbf166530794990b7c4d9b88b326
dishant <aryadishant414@gmail.com>
2024-08-23 09:08:45 +0530

    Completed top section

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,5 +1,68 @@
+import styled from 'styled-components';
+
 const App = () => {
-  return <div>App</div>;
-};
+  return ( 
+    <MainContainer>
+
+      <TopContainer>
+        <div className='logo'>
+          <img src="/logo.svg" alt="logo" />
+        </div>
+        <div className='search'>
+          <input placeholder="Search Food" />
+        </div>
+      </TopContainer>
+
+      <FilterContainer>
+        <Button>All</Button>
+        <Button>Breakfast</Button>
+        <Button>Lunch</Button>
+        <Button>Dinner</Button>
+      </FilterContainer>
+
+    </MainContainer>
+  );
+  };
 
 export default App;
+
+const MainContainer = styled.div`
+  /* border: 2px solid red; */
+  max-width: 1200px;
+  margin: 0 auto;
+`
+const TopContainer = styled.section`
+  display: flex;
+  justify-content: space-between;
+  min-height: 140px;
+  padding: 16px;
+  align-items: center;
+
+  .search {
+    input {
+      background-color: transparent;
+      border: 1px solid red;
+      color: white;
+      border-radius: 5px;
+      height: 40px;
+      font-size: 16px;
+      padding: 10px 15px;
+    }
+  }
+`;
+
+const FilterContainer = styled.section`
+  display: flex;
+  justify-content: center;
+  gap: 14px;
+`;
+
+const Button = styled.button`
+  background-color: #FF4343;
+  padding: 6px 12px;
+  gap: 10px;
+  height: 31px;
+  border-radius: 5px;
+  border: none;
+  color: white;
+`;
--- app/src/main.jsx
+++ app/src/main.jsx
@@ -2,8 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
+// globalStyles.js
+import { createGlobalStyle } from 'styled-components';
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  body {
+    background-color: #323334;
+    color: white;
+    min-height: 100vh;
+  }
+`;
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
